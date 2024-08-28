cbe60b611cd7a670154945401627116a89803aa1
JromarioSs <jromario2014@gmail.com>
2024-08-28 19:48:50 -0300

    Criando a primeira API em .NET

---
+++ modulo01/MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Retorno), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult Get()
+    {
+        Retorno res = new Retorno()
+        {
+            Id = 1,
+            Nome = "Romario"
+        };
+        return Ok(res);
+    }
+
+    public class Retorno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
